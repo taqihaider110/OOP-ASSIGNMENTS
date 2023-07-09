d32ce663da8ec1f511111ac7f1754233b92f3904
Taqi haider <taqihaider591@gmail.com>
2023-07-09 21:57:49 +0500

    push

--- OOP ASSIGNMENT 2/Q4.cpp
+++ OOP ASSIGNMENT 2/Q4.cpp
@@ -1,118 +1,83 @@
 #include <iostream>
-#include <cstring>
+#include<cstring>
 using namespace std;
-
-class InvalidDataException
-{
+class CustomException{
 private:
-    string message;
-
+    string errorMessage;
 public:
-    InvalidDataException(const string &errorMessage) : message(errorMessage) {}
-    string getMessage() const
-    {
-        return message;
+    CustomException(const string& message) : errorMessage(message) {}
+    string getMessage() const{
+        return errorMessage;
     }
 };
-
-class Person
-{
+class Person{
 protected:
     string name;
     int age;
-
 public:
     Person(const string& personName, int personAge) : name(personName), age(personAge) {}
-    virtual void display() const = 0;
-    string getName() const
-    {
+    virtual void displayInfo() const = 0;
+    string getName() const{
         return name;
     }
 };
-
-class Student : public Person
-{
+class Student : public Person{
 private:
     string studentId;
-
 public:
     Student(const string& studentName, int studentAge, const string& id)
         : Person(studentName, studentAge), studentId(id) {}
-    void display() const override
-    {
+    void displayInfo() const override{
         cout << "Student Name: " << name << ", Age: " << age << ", Student ID: " << studentId << endl;
     }
 };
-
-class Professor : public Person
-{
+class Professor : public Person{
 private:
     string course;
-
 public:
     Professor(const string& professorName, int professorAge, const string& professorCourse)
         : Person(professorName, professorAge), course(professorCourse) {}
-    void display() const override
-    {
+    void displayInfo() const override{
         cout << "Professor Name: " << name << ", Age: " << age << ", Course: " << course << endl;
     }
 };
-
-class Staff : public Person
-{
+class Staff : public Person{
 private:
-    string role;
-
+    string designation;
 public:
     Staff(const string& staffName, int staffAge, const string& staffRole)
-        : Person(staffName, staffAge), role(staffRole) {}
-    void display() const override
-    {
-        cout << "Staff Name: " << name << ", Age: " << age << ", Role: " << role << endl;
+        : Person(staffName, staffAge), designation(staffRole) {}
+    void displayInfo() const override{
+        cout << "Staff Name: " << name << ", Age: " << age << ", Designation: " << designation << endl;
     }
 };
-
 template <class T>
-class Database
-{
+class Database{
 private:
-    T **data;
+    T** data;
     int capacity;
     int size;
-
 public:
-    Database(int initialCapacity = 10) : capacity(initialCapacity), size(0)
-    {
-        data = new T *[capacity];
+    explicit Database(int initialCapacity = 10) : capacity(initialCapacity), size(0){
+        data = new T*[capacity];
     }
-
-    ~Database()
-    {
-        clear();
+    ~Database(){
+        remove();
         delete[] data;
     }
-
-    void addRecord(T *record)
-    {
-        if (size == capacity)
-        {
-            throw InvalidDataException("Database is full. Cannot add more records.");
+    void Add_Record(T* record){
+        if (size == capacity){
+            throw CustomException("Database is full. Cannot add more records.");
         }
-
         data[size] = record;
         size++;
     }
-
-    bool removeRecord(T *record)
-    {
-        for (int i = 0; i < size; i++)
-        {
-            if (data[i] == record)
-            {
+    bool Remove_Record(T* record){
+        for (int i = 0; i < size; i++){
+            if (data[i] == record){
                 delete data[i];
                 size--;
-                for (int j = i; j < size; j++)
-                {
+                for (int j = i; j < size; j++){
                     data[j] = data[j + 1];
                 }
                 return true;
@@ -120,78 +85,58 @@ public:
         }
         return false;
     }
-
-    T *searchRecord(const string& name) const
-    {
-        for (int i = 0; i < size; i++)
-        {
-            if (data[i]->getName() == name)
-            {
+    T* Search_Record(const string& name) const{
+        for (int i = 0; i < size; i++){
+            if (data[i]->getName() == name){
                 return data[i];
             }
         }
         return nullptr;
     }
-
-    void displayAllRecords() const
-    {
-        for (int i = 0; i < size; i++)
-        {
-            data[i]->display();
+    void Displaying_Records() const{
+        for (int i = 0; i < size; i++){
+            data[i]->displayInfo();
         }
     }
-
-    void clear()
-    {
-        for (int i = 0; i < size; i++)
-        {
+    void remove(){
+        for (int i = 0; i < size; i++){
             delete data[i];
         }
         size = 0;
     }
 };
-
-int main()
-{
-    try
-    {
-        Database<Person> database;
-
-        Person *student1 = new Student("Taqi", 20, "CT-092");
-        Person *student2 = new Student("Wajihn-ur-Rehman", 20, "CT-084");
-        Person *professor = new Professor("Sir Furqan", 35, "Computer Science");
-        Person *staff = new Staff("Ali", 28, "Floor incharge");
-
-        database.addRecord(student1);
-        database.addRecord(student2);
-        database.addRecord(professor);
-        database.addRecord(staff);
-
-        cout << "Displaying all records:" << endl;
-        database.displayAllRecords();
-
-        Person *foundRecord = database.searchRecord("Taqi");
-        if (foundRecord)
-        {
-            cout << "Found record:" << endl;
-            foundRecord->display();
+int main(){
+    try{
+        Database<Person> db;
+        Person* student1 = new Student("Taqi Haider", 20, "CT-092");
+        Person* student2 = new Student("Wajih-ur -rehman", 20, "CT-083");
+        Person* student3 = new Student("Abbas Haider", 19, "CE-121");
+        Person* professor = new Professor("Sir Furqan", 35, "Computer Science");
+        Person* staff = new Staff("Michael Johnson", 28, "Floor Manager");
+        db.Add_Record(student1);
+        db.Add_Record(student2);
+        db.Add_Record(student3);
+        db.Add_Record(professor);
+        db.Add_Record(staff);
+        cout << "---------------------Displaying all records-------------------" << endl;
+        db.Displaying_Records();
+        Person* foundRecord = db.Search_Record("Taqi");
+        if (foundRecord){
+            cout << "The Record you enter is Found:" << endl;
+            foundRecord->displayInfo();
         }
-        else
-        {
-            cout << "Record not found." << endl;
+        else{
+            cout << "Record not found. " << endl;
         }
-
-        database.removeRecord(staff);
+        db.Remove_Record(staff);
         cout << "After removing a record:" << endl;
-        database.displayAllRecords();
-
-        database.clear();
-
+        db.Displaying_Records();
+        db.remove();
         return 0;
     }
-    catch (const InvalidDataException &e)
+    catch (const CustomException& t)
     {
-        cout << "Invalid Data: " << e.getMessage() << endl;
+        cout << "The Error of the code is : " << t.getMessage() << endl;
         return 1;
     }
 }
