4a928aa6d519064e9a4cf4b8b17c49779300682d
Taqi haider <taqihaider591@gmail.com>
2023-07-09 20:54:09 +0500

    Practice

--- OOP ASSIGNMENT 2/1.cpp
+++ OOP ASSIGNMENT 2/1.cpp
@@ -1,6 +1,5 @@
 #include<iostream>
 #include<cstdlib>
-#include<ctime>
 
 using namespace std;
 
@@ -8,6 +7,7 @@ class Book{
 	private:
 		string bookname,author;
 	public:
+		Book():bookname(""),author(""){}
 		Book(string b,string a):bookname(b),author(a){}
 		void setbookname(string n){bookname=n;}
 		void setauthor(string a){author=a;}
@@ -15,6 +15,7 @@ class Book{
 		string getauthor(){return author;}
 		virtual void takebook()=0;
 		virtual void display()=0;
+		virtual void set()=0;
 };
 
 class Textbook:public Book{
@@ -22,11 +23,18 @@ class Textbook:public Book{
 		bool Available=true;
 	public:
 		Textbook(string n,string a):Book(n,a){}
+		Textbook():Book(){}
+		void set(){
+			string b,a;
+			cout<<"Enter Book Name, Author"<<endl;
+			cin>>b>>a;
+			setbookname(b);setauthor(a);
+		}
 		void takebook(){
 			Available=false;
 		}
 		void display(){
-			cout<<getbookname()<<endl<<getauthor()<<endl;
+			cout<<"Book Name:"<<getbookname()<<" | Author:"<<getauthor()<<endl;
 		}
 };
 
@@ -35,25 +43,34 @@ class Novel:public Book{
 		string genre;
 	public:
 		Novel(string n,string g,string a):Book(n,a),genre(g){}
+		Novel():Book(),genre(""){}
+		void set(){
+			string b,a,g;
+			cout<<"Enter Book Name, Author and Genre"<<endl;
+			cin>>b>>a>>g;
+			setbookname(b);setauthor(a);genre=g;
+		}
 		void takebook(){
-			cout<<genre<<" Novel was bought"<<endl;
+			cout<<genre<<" Novel was taken"<<endl;
 		}
 		void display(){
-			cout<<"Book Name:"<<getbookname()<<endl<<"Genre:"<<genre<<endl<<"Author:"<<getauthor()<<endl;
+			cout<<"Book Name:"<<getbookname()<<" | Genre:"<<genre<<" | Author:"<<getauthor()<<endl;
 		}
 };
 
 class LibraryMember{
 	private:
 		string name;
-		time_t datejoined;
+		bool canborrow=true;
 	public:	
-		LibraryMember(string n):name(n),datejoined(time(0)){}
+		LibraryMember(string n):name(n){}
+		LibraryMember():name(""){}
 		void setname(string n){name=n;}
-		void setdate(){time(0);}
+		void toggleborrow(){canborrow = !canborrow;}
+		bool borrow(){return canborrow;}
 		string getname(){return name;}
-		time_t getdate(){return datejoined;}
 		virtual void display()=0;
+		virtual void Set()=0;
 };
 
 class Student:public LibraryMember{
@@ -61,8 +78,15 @@ class Student:public LibraryMember{
 		int RollNo;
 	public:
 		Student(string n,int r):LibraryMember(n),RollNo(r){}
+		Student():LibraryMember(),RollNo(0){}
+		void Set(){
+			string n;int r;
+			cout<<"Enter Name and RollNo."<<endl;
+			cin>>n>>r;
+			setname(n);RollNo=r;
+		}
 		void display(){
-			cout<<"Name :"<<getname()<<endl<<"Date Joined :"<<getdate()<<endl<<"Roll No :"<<RollNo<<endl;
+			cout<<"Student | Name :"<<getname()<<" | Roll No :"<<RollNo<<endl;
 		}
 };
 
@@ -71,8 +95,16 @@ class Faculty:public LibraryMember{
 		string Department;
 	public:	
 		Faculty(string n,string D):LibraryMember(n),Department(D){}
+		Faculty():LibraryMember(),Department(""){}
+		void Set(){
+			string n;int r;
+			cout<<"Enter Name and Dept."<<endl;
+			cin>>n;
+			cin>>Department;
+			setname(n);
+		}
 		void display(){
-			cout<<"Name :"<<getname()<<endl<<"Date Joined :"<<getdate()<<endl<<"Department :"<<Department<<endl;
+			cout<<"Faculty | Name :"<<getname()<<" | Department :"<<Department<<endl;
 		}
 };
 
@@ -80,35 +112,150 @@ class Transaction{
 	private:
 		int ID;
 		int amount;
-		time_t date;
 	public:
-		Transaction():ID(rand()),amount(0),date(time(0)){}
+		Transaction():ID(rand()),amount(0){}
 		void setamount(int a){amount=a;}
-		void setdate(){time(0);}
 		int getamount(){return amount;}
-		time_t getdate(){return date;}
-		void GetBook(Book& book,LibraryMember& person){
-			book.display();
-			cout<<"was Successfuly taken by:"<<endl;
-			person.display();
-			book.takebook();
-			cout<<"Enter amount for transaction: "<<endl;
-			cin>>amount;
-			date=time(0);
+		bool GetBook(Book& book,LibraryMember& person){
+			if(person.borrow()){
+				book.display();
+				cout<<"was Successfuly borrowed by:"<<endl;
+				person.display();
+				book.takebook();
+				amount=0;
+				receipt();
+				return false;
+			}
+			else{
+				book.display();
+				cout<<"was Successfuly taken by:"<<endl;
+				person.display();
+				book.takebook();
+				cout<<"Enter amount for transaction: "<<endl;
+				cin>>amount;
+				receipt();
+				return true;
+			}
 		}
 		void receipt(){
-			cout<<"transaction ID: "<<ID<<endl<<"Amount :"<<amount<<endl<<"Date of Transaction :"<<time(0)<<endl;
+			cout<<"transaction ID: "<<ID<<endl<<"Amount :"<<amount<<endl<<endl;
 		}
 		
 };
 
+class Library{
+	Book** books;
+	LibraryMember** people;
+	int Nbooks,Npeople;
+	public:
+		Library(int b,int p):Nbooks(b),Npeople(p){
+			int opt;
+			books= new Book*[b];
+			for(int i=0;i<b;i++){
+				cout<<"Is book a (1) Textbook or (2) Novel :"<<endl;
+				cin>>opt;
+				while(opt!=1&&opt!=2){
+					cout<<"Invalid option, try again"<<endl;
+					cout<<"Is book a (1) Textbook or (2) Novel :"<<endl;
+					cin>>opt;
+				}
+				if(opt==1){
+					books[i]= new Textbook;
+					books[i]->set();
+				}
+				else if(opt==2){
+					books[i]= new Novel;
+					books[i]->set();
+				}
+			}
+			cout<<"----------------------------"<<endl;
+			people= new LibraryMember*[p];
+			for(int i=0;i<p;i++){
+				cout<<"Is person a (1)Faculty or (2)Student :"<<endl;
+				cin>>opt;
+				while(opt!=1&&opt!=2){
+					cout<<"Invalid option, try again"<<endl;
+					cout<<"Is person a (1)Faculty or (2)Student :"<<endl;
+					cin>>opt;
+				}
+				if(opt==1){
+					people[i]= new Faculty;
+					people[i]->Set();
+				}
+				else if(opt==2){
+					people[i]= new Student;
+					people[i]->Set();
+				}
+			}
+		}
+		void Show(){
+			cout<<endl<<endl<<endl<<"////////Library Details/////////"<<endl;
+			cout<<"Total Books = "<<Nbooks<<endl;
+			for(int i=0;i<Nbooks;i++){
+				cout<<"INDEX #"<<i<<"\t";
+				books[i]->display();
+				cout<<endl;
+			}
+			cout<<"Total People = "<<Npeople<<endl;
+			for(int i=0;i<Npeople;i++){
+				cout<<"INDEX #"<<i<<"\t";
+				people[i]->display();
+				cout<<endl;
+			}
+		}
+		void Addmember(){
+			LibraryMember** x;
+			x=new LibraryMember*[Npeople];
+			for(int i=0;i<Npeople;i++){
+				x[i]=people[i];
+			}
+			people = new LibraryMember*[Npeople+1];
+			for(int i=0;i<Npeople;i++){
+				people[i]=x[i];
+			}
+			people[Npeople]->Set();
+			Npeople++;
+		}
+		void AddBook(){
+			Book** x;
+			x=new Book*[Nbooks];
+			for(int i=0;i<Nbooks;i++){
+				x[i]=books[i];
+			}
+			books = new Book*[Nbooks+1];
+			for(int i=0;i<Nbooks;i++){
+				books[i]=x[i];
+			}
+			books[Nbooks]->set();
+			Nbooks++;
+		}
+		void take(){
+			Transaction t;
+			cout<<"Enter your Index in Library List, and Index of Book"<<endl;
+			int i,j;
+			cin>>i>>j;
+			if(i<0||i>=Npeople||j<0||j>Nbooks){
+				cout<<"Invalid Transaction, aborting"<<endl;
+				return;
+			}
+			if(t.GetBook(*books[i],*people[j])){
+				cout<<"Book was Bought, total books decremented"<<endl;
+				Nbooks--;
+			}
+			else{
+				cout<<"Book was Borrowed only "<<endl;
+			}
+		}
+};
 int main(){
-	Student st("Murtaza",93);
-	Novel novel("How to Kill Someone","Thriller","Jason Mamoa");
-	cout<<"Generating Transaction..."<<endl;
-	Transaction obj;
-	obj.GetBook(novel,st);
-	obj.receipt();
+	int n,p;
+	cout<<"Welcome to the library"<<endl;
+	cout<<"Enter Number of Total Books, and Total People :"<<endl;
+	cin>>n>>p;
+	Library l(n,p);
 	
+	l.Show();
+	l.take();
+	l.Show();
 	
 }
--- OOP ASSIGNMENT 2/Q1.cpp
+++ OOP ASSIGNMENT 2/Q1.cpp
@@ -6,72 +6,24 @@ class Book{
     string name,author;
     public:
     Book(const string &name, const string &author): name(name), author(author){}
-    // virtual void add_book()=0;
-    // virtual void remove_book()=0;
-    // virtual void update_book()=0;
-    virtual void display_book()=0;
+    virtual void display_book() const =0;
 };
 class Textbook:public Book{
     string Subject;
     public:
-    Textbook(const string &name,string &author,string &Subject):Book(name,author),Subject(Subject){}
-    // void add_book() override{
-    //     cout<<"-------Enter Details for TextBook--------"<<endl;
-    //     cout<<"Enter TextBook name :"<<endl;
-    //     getline(cin>>ws, name);
-    //     cout<<"Enter Author name :"<<endl;
-    //     getline(cin>>ws, author);
-    // }
-    // void remove_book() override{
-    //     cout<<"Enter TextBook name to remove :"<<endl;
-    //     getline(cin>>ws, name);
-    //     cout<<"Enter Author name to remove :"<<endl;
-    //     getline(cin>>ws, author);
-    //     name = " ";
-    //     author = " ";
-    //     cout<<"Book removed successfully."<<endl;
-    // }
-    // void update_book() override {
-    //     cout<<"Enter TextBook name to update :"<<endl;
-    //     getline(cin>>ws, name);
-    //     cout<<"Enter Author name to update :"<<endl;
-    //     getline(cin>>ws, author);
-    // }
-    void display_book() override{
+    Textbook(const string &name,const string &author,const string &Subject):Book(name,author),Subject(Subject){}
+    void display_book() const override{
         cout<<"-------Details for TextBook--------"<<endl;
         cout<<"Text Book name : "<<name<<endl;
         cout<<"Author name : "<<author<<endl;
         cout<<"Subject name : "<<Subject<<endl;
     }
-};
-
+};  
 class Novel:public Book{
     string type;
     public:
-    Novel(const string &name,string &author,string &type):Book(name,author),type(type){}
-    // void add_book() override{
-    //     cout<<"-------Enter Details for Novel--------"<<endl;
-    //     cout<<"Enter Novel name :"<<endl;
-    //     getline(cin>>ws, name);
-    //     cout<<"Enter Novel Author name :"<<endl;
-    //     getline(cin>>ws, author);
-    // }
-    // void remove_book() override{
-    //     cout<<"Enter Novel name to remove:"<<endl;
-    //     getline(cin>>ws, name);
-    //     cout<<"Enter Novel Author name to remove :"<<endl;
-    //     getline(cin>>ws, author);
-    //     name = " ";
-    //     author = " ";
-    //     cout<<"Book removed successfully."<<endl;
-    // }
-    // void update_book() override{
-    //     cout<<"Enter Novel name to update:"<<endl;
-    //     getline(cin>>ws, name);
-    //     cout<<"Enter Novel Author name to update :"<<endl;
-    //     getline(cin>>ws, author);
-    // }
-    void display_book() override{
+    Novel(const string &name,const string &author,const string &type):Book(name,author),type(type){}
+    void display_book() const override{
         cout<<"-------Details for Novel--------"<<endl;
         cout<<"Novel name : "<<name<<endl;
         cout<<"Novel Author name : "<<author<<endl;
@@ -84,70 +36,238 @@ class LibraryMember{
     string name;
     public:
     LibraryMember(int Id,const string &name):Id(Id),name(name){}
-    // virtual void get_data()=0;
     virtual void display_data()=0;
-
 };
 class StudentMember:public LibraryMember{
     private:
     int Max_books;
+    int borrowed_books_count;
+    Book* borrowed_books[5];
     public:
-    StudentMember(int Id,const string &name):LibraryMember(Id,name),Max_books(5){}
-    // void get_data(){
-    //     cout<<"-------Enter Details for Student Member--------"<<endl;
-    //     cout<<"Enter Student member Id :"<<endl;
-    //     cin>>Id;
-    //     cout<<"Enter Student member Name :"<<endl;
-    //     getline(cin>>ws, name);
-    //     }
+    StudentMember(int Id,const string &name):LibraryMember(Id,name),Max_books(5),borrowed_books_count(0){}
     void display_data(){
         cout<<"-------Details for Student Member--------"<<endl;
         cout<<"Student member Id :"<<Id<<endl;
         cout<<"Student member Name :"<<name<<endl;
         cout<<"Maximum Books Allowed :"<<Max_books<<endl;
         }
+    // void borrow_book(Book* book) {
+    //     if (borrowed_books_count < Max_books) {
+    //         borrowed_books[borrowed_books_count++] = book;
+    //         cout << "Book \"" << book->name << "\" borrowed successfully." << endl;
+    //     }
+    //     else {
+    //         cout << "Maximum book limit reached. Cannot borrow more books." <<endl;
+    //     }
+    // }
+    // void return_book(Book* book) {
+    //     for (int i = 0; i < borrowed_books_count; i++) {
+    //         if (borrowed_books[i] == book) {
+    //             borrowed_books[i] = borrowed_books[--borrowed_books_count];
+    //             cout << "Book \"" << book->name << "\" returned successfully." <<endl;
+    //         }
+    //     }
+    //     cout << "Book not found in the borrowed books list." << endl;
+    // }
 };
 class FacultyMember:public LibraryMember{
     private:
     int Max_books;
+    int borrowed_books_count;
+    Book* borrowed_books[5];
     public:
     FacultyMember(int Id,const string &name):LibraryMember(Id,name),Max_books(5){}
-    // void get_data(){
-    //     cout<<"-------Enter Details for Faculty Member--------"<<endl;
-    //     cout<<"Enter Faculty member Id :"<<endl;
-    //     cin>>Id;
-    //     cout<<"Enter Faculty member Name :"<<endl;
-    //     getline(cin>>ws, name);
-    //     cout<<"Enter Faculty member Designation :"<<endl;
-    //     getline(cin>>ws, designation);
-    //     }
     void display_data(){
         cout<<"-------Details for Faculty Member--------"<<endl;
         cout<<"Faculty member Id :"<<Id<<endl;
         cout<<"Faculty member Name :"<<name<<endl;
         cout<<"Maximum Books Allowed :"<<Max_books<<endl;
         }
+    // void borrow_book(Book* book) {
+    //     if (borrowed_books_count < Max_books) {
+    //         borrowed_books[borrowed_books_count++] = book;
+    //         cout << "Book \"" << book->name << "\" borrowed successfully." << endl;
+    //     }
+    //     else {
+    //         cout << "Maximum book limit reached. Cannot borrow more books." << endl;
+    //     }
+    // }
+
+    // void return_book(Book* book) {
+    //     for (int i = 0; i < borrowed_books_count; i++) {
+    //         if (borrowed_books[i] == book) {
+    //             borrowed_books[i] = borrowed_books[--borrowed_books_count];
+    //             cout << "Book \"" << book->name << "\" returned successfully." << endl;
+    //             return;
+    //         }
+    //     }
+    //     cout << "Book not found in the borrowed books list." << endl;
+    // }
 };
 class Transaction{
     Book* book;
     LibraryMember* member;
     string due_date;
     public:
-    Book* book;
-    LibraryMember* member;
-    Transaction():book(nullptr),member(nullptr),due_date(""){}
     Transaction(Book* book,LibraryMember* member,const string &due_date):
     book(book),member(member),due_date(due_date){}
-    Book *getBook(){
+    Book* getBook(){
+        return book;
     }
-    LibraryMember *getMember(){
+    LibraryMember* getMember(){
+        return member;
     }
     string getDueDate(){
         return due_date;
     }
 };
-class library{
+class Library{
+    static const int MAX_BOOKS = 100;
+    static const int MAX_MEMBERS = 100;
+    static const int MAX_TRANSACTIONS = 100;
+    Book* books[MAX_BOOKS];
+    LibraryMember* members[MAX_MEMBERS];
+    Transaction* transactions[MAX_TRANSACTIONS];
+    int numBooks;
+    int numMembers;
+    int numTransactions;
+    int findBookIndex(Book* book) const{
+        for (int i = 0; i < numBooks; ++i){
+            if (books[i] == book){
+                return i;
+            }
+        }
+        return -1;
+    }
+    int findMemberIndex(LibraryMember* member) const{
+        for (int i = 0; i < numMembers; ++i){
+            if (members[i] == member){
+                return i;
+            }
+        }
+        return -1;
+    }
+    int findTransactionIndex(Book* book, LibraryMember* member) const{
+        for (int i = 0; i < numTransactions; ++i){
+            if (transactions[i]->getBook() == book && transactions[i]->getMember() == member){
+                return i;
+            }
+        }
+        return -1;
+    }
+    public:
+    Library(): numBooks(0), numMembers(0), numTransactions(0){}
+    void addBook(Book* book){
+        if (numBooks < MAX_BOOKS){
+            books[numBooks++] = book;
+            cout << "Book added successfully." << endl;
+        }
+        else{
+            cout << "Cannot add more books. Maximum limit reached." << endl;
+        }
+    }
+    void removeBook(Book* book){
+        int index = findBookIndex(book);
+        if (index != -1){
+            for (int i = index; i < numBooks - 1; ++i){
+                books[i] = books[i + 1];
+            }
+            numBooks--;
+            cout << "Book removed successfully." << endl;
+        }
+        else{
+            cout << "Book not found." << endl;
+        }
+    }
+    void addMember(LibraryMember* member){
+        if (numMembers < MAX_MEMBERS){
+            members[numMembers++] = member;
+            cout << "Member added successfully." << endl;
+        }
+        else{
+            cout << "Cannot add more members. Maximum limit reached." << endl;
+        }
+    }
+    void removeMember(LibraryMember* member){
+        int index = findMemberIndex(member);
+        if (index != -1){
+            for (int i = index; i < numMembers - 1; ++i){
+                members[i] = members[i + 1];
+            }
+            numMembers--;
+            cout << "Member removed successfully." << endl;
+        }
+        else{
+            cout << "Member not found." << endl;
+        }
+    }
+    void borrowBook(LibraryMember* member, Book* book, const string &dueDate){
+        int memberIndex = findMemberIndex(member);
+        int bookIndex = findBookIndex(book);
+
+        if (memberIndex != -1 && bookIndex != -1){
+            if (numTransactions < MAX_TRANSACTIONS){
+                Transaction* transaction = new Transaction(book, member, dueDate);
+                transactions[numTransactions++] = transaction;
+                cout << "Book borrowed successfully." << endl;
+            }
+            else{
+                cout << "Cannot perform the borrowing operation. Maximum transaction limit reached." << endl;
+            }
+        }
+        else{
+            cout << "Member or book not found." << endl;
+        }
+    }
+    void returnBook(Book* book, LibraryMember* member){
+        int transactionIndex = findTransactionIndex(book, member);
+        if (transactionIndex != -1)
+        {
+            for (int i = transactionIndex; i < numTransactions - 1; ++i){
+                transactions[i] = transactions[i + 1];
+            }
+            numTransactions--;
+            cout << "Book returned successfully." << endl;
+        }
+        else{
+            cout << "Transaction not found." << endl;
+        }
+    }
+    void printAllBooks() const
+    {
+        for (int i = 0; i < numBooks; ++i)
+        {
+            books[i]->display_book();
+            cout << endl;
+        }
+    }
+    void printAllMembers() const
+    {
+        for (int i = 0; i < numMembers; ++i)
+        {
+            members[i]->display_data();
+            cout << endl;
+        }
+    }
 };
 int main(){
+    Library Lib;
+
+    Book* book1 = new Textbook("In search of Lost time", "Marcel Proust", "Biography");
+    Book* book2 = new Novel("Ulysses", "James Joyce", "Thriller");
+    Lib.addBook(book1);
+    Lib.addBook(book2);
+
+    LibraryMember* member1 = new StudentMember(7574, "Taqi Haider");
+    LibraryMember* member2 = new FacultyMember(214421, "Wajih-ur-Rehman");
+    Lib.addMember(member1);
+    Lib.addMember(member2);
+
+    Lib.printAllBooks();
+    Lib.printAllMembers();
+
+    Lib.borrowBook(member1, book1, "09-07-2023");
+    Lib.borrowBook(member2, book2, "09-07-2023");
+    Lib.returnBook(book1, member1);
     return 0;
 }
