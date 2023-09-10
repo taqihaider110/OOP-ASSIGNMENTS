8f9e666fe8da589de92340da4e2a6e94b6a76a9f
Taqi haider <taqihaider591@gmail.com>
2023-09-10 19:31:06 +0500

    push

---
+++ SUMMERS OOC&P/LABQ2.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+float height;
+cout<<"Enter Height In Inches: ";
+cin>>height;
+height = height/12;
+cout<<"This is equivalent to "<<height<<endl;
+}
